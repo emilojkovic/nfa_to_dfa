77c7963e33b685ea3f433174ce748efa53c1ec48
Ema Milojkovic <milojkovic.ema@gmail.com>
2018-02-13 15:12:34 -0600

    refactored and commented code

--- dfa_generator.py
+++ dfa_generator.py
@@ -10,31 +10,45 @@ class DFA_Generator:
     self.dfa_transition_1 = []
     self.dfa_accepting = []
 
-  def get_epsilon_reaches(self, state):
-    queue = [state]
+  '''
+  Input:      a state in our DFA (representing a set of NFA states)
+  Output:     None
+
+  Uses BFS to find every node within a node's epsilon reach
+  (including itself and nodes that are more than one epsilon
+  transition away)
+  '''
+  def get_epsilon_reaches(self, dfa_state):
+    queue = [dfa_state]
     visited = set()
-    visited.add(state)
+    visited.add(dfa_state)
     reachable = set()
 
     while queue:
-      p = queue.pop(0)
-      #print("Full state string: {}".format(p))
-      for current_state in p:
-       # print("state string: {}".format(current_state))
-       #current_state = queue.pop(0)
-        if "epsilon" in self.nfa.transitions[current_state]:
-          ereach = self.nfa.transitions[current_state]["epsilon"]
-          #print("within epsilon reach: {}".format(ereach))
-          for e in ereach:
-            if e not in visited:
-              queue.append(e)
-              visited.add(e)
-            reachable.add(e)
+      nfa_states = queue.pop(0)
+     
+      # iterates through set of NFA states that represents a DFA state
+      for state in nfa_states:
+        if "epsilon" in self.nfa.transitions[state]:
+          # get all states reachable via epsilon transitions
+          ereach = self.nfa.transitions[state]["epsilon"]
+          for reachable_state in ereach:
+            if reachable_state not in visited:
+              queue.append(reachable_state)
+              visited.add(reachable_state)
+            reachable.add(reachable_state)
     
-    self.epsilon_reaches[state] = reachable
-    #print("Generated epsilon reaches for state [{}]: {}".format(state, reachable))
+    self.epsilon_reaches[dfa_state] = reachable
+    #print("Generated epsilon reaches for state [{}]: {}".format(dfa_state, reachable))
 
 
+  '''
+  Input:      None
+  Output:     None
+
+  Uses Incremental Subset Algorithm to generate all DFA states 
+  and transision given an NFA
+  '''
   def incremental_subset_algo(self):
     queue = [self.nfa.start_state]
     visited = set()
@@ -42,15 +56,14 @@ class DFA_Generator:
 
     while queue:
       state = queue.pop(0)
-      #print("CURRENT STATE: {}".format(state))
-      
       self.dfa_q.append(state)
 
-      if state not in self.epsilon_reaches:
+      if state not in self.epsilon_reaches: 
         self.get_epsilon_reaches(state)
       epsilon_reach = self.epsilon_reaches[state]
       self.dfa_epsilon_reach.append(epsilon_reach)
 
+      # finding all nodes reachable with "0" transition from NFA nodes in DFA state
       zero_reachable = set()
       for e in epsilon_reach:
         if "0" in self.nfa.transitions[e]:
@@ -58,6 +71,7 @@ class DFA_Generator:
             zero_reachable.add(zero_reach)
       self.dfa_transition_0.append(zero_reachable)
 
+      # finding all nodes reachable with "1" transition from NFA nodes in DFA state 
       one_reachable = set()
       for e in epsilon_reach:
         if "1" in self.nfa.transitions[e]:
@@ -65,25 +79,14 @@ class DFA_Generator:
             one_reachable.add(one_reach)
       self.dfa_transition_1.append(one_reachable)
 
+      # creates new DFA state with string of all NFA nodes from zero reach
       zero_state =  "".join(zero_reachable) 
       if zero_state not in visited and zero_state is not '':
         queue.append(zero_state)
         visited.add(zero_state)
-
+      
+      # creates new DFA state with string of all NFA nodes from one reach
       one_state =  "".join(one_reachable) 
       if one_state not in visited and one_state is not '':
         queue.append(one_state)
         visited.add(one_state)
-
-
-
-
-
-
-      
-      
-
-      
-
-
-    
--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 from graph import Graph
 from dfa_generator import DFA_Generator
 import json
+import sys
 
-data = json.load(open('nfa_2b.json'))
+# sets input and output files
+in_file = open(sys.argv[1], 'r')
+out_file = open("dfa_output.txt", 'w')
+
+#parses input NFA json to create an NFA graph
+data = json.load(in_file)
 start = data["start"]
 accepting = data["accepting"]
 transitions = data["transitions"]
-
 nfa = Graph(start, accepting, transitions)
+
+# creates DFA Generator based on NFA graph
 dfa = DFA_Generator(nfa)
 
-#dfa.get_epsilon_reaches("fq")
+# runs Incremental Subset Algorithm on NFA to produce DFA
 dfa.incremental_subset_algo()
-print("DFA generated from NFA using incremental subset algorithm.")
-print("DFA states: {}".format(dfa.dfa_q))
-print("The number of reachable DFA states is {}.".format(len(dfa.dfa_q)))
+
+# writes resulting DFA in output file
+out_file.write("DFA generated from NFA in [{}] using incremental subset algorithm.\n\n".format(sys.argv[1]))
+states = dfa.dfa_q
+num_states = len(dfa.dfa_q)
+
+e_reach = dfa.dfa_epsilon_reach
+transition_0 = dfa.dfa_transition_0
+transition_1 = dfa.dfa_transition_1
+
+out_file.write("The number of reachable DFA states is {}.\n".format(num_states))
+out_file.write("\n")
+
+for i in range(num_states):
+  out_file.write("DFA states: {}\n".format(states[i]))
+  out_file.write("---------- e reach: {}\n".format(e_reach[i]))
+  out_file.write("----- transition 0: {}\n".format(transition_0[i]))
+  out_file.write("----- transition 1: {}\n".format(transition_1[i]))
+  out_file.write("\n")
 
